39a7cddc85ce20f28b62c7b13cf53abd8759f946
yuvarajancitspl <yuvarajan.citspl@gmail.com>
2020-05-11 17:34:56 +0530

    UI added, readme file updated

--- README.md
+++ README.md
@@ -2,7 +2,17 @@ Splunk remote query executor
 
 This repo contains a python file 'search.py'
 
-prerequisites python2.7, splunk-sdk==1.6.12
+prerequisites python3.6
+
+Create virtual environment(To avoid conflict between other project packages)
+
+    1) open terminal in ubuntu
+    2) run sudo apt-get install python3-tk
+    3) run git clone https://github.com/yuvarajanv/splunk_remote_search.git
+    2) run virtualenv -v python3.6 venv
+    3) run . venv/bin/activate
+    4) run pip install -r requirement.txt
+    5) run python search.py
 
 Input required:
     
@@ -13,16 +23,5 @@ Input required:
     time range to compare: ex = 24h
     search key: ex = index="_internal" | top sourcetype
 
-
-To run:
-
-pip install -r requirement.txt
-
-python search.py
-
-
 Output: 
-    The current output will be the list of search result.
-
-
-TO DO: trigger a mail about the result in JSON format.
+    The output will be stored in file. The file name will be suffixed withe the current time
--- search.py
+++ search.py
@@ -1,35 +1,72 @@
-import logging
+import os
 import json
+from datetime import datetime
 
 import splunklib.client as client
 import splunklib.results as results
-logger = logging.getLogger(__name__)
+from tkinter import Tk, Label, Entry, mainloop, Button
 
-HOST = raw_input('enter server IP address : ')
-PORT = int(raw_input('enter server PORT : '))
-USERNAME = raw_input('enter user name : ')
-PASSWORD = raw_input('enter password : ')
-earliest_time = raw_input('time range : ')
-search_key = raw_input('enter search query : ')
 
-service = client.connect(
-    host=HOST, port=PORT, username=USERNAME, password=PASSWORD)
+def init_ui():
+    master = Tk()
+    Label(master, text='Server IP address : ').grid(row=0)
+    Label(master, text='Server PORT number : ').grid(row=1)
+    Label(master, text='User Name : ').grid(row=2)
+    Label(master, text='Password : ').grid(row=3)
+    Label(master, text='Time Range : ').grid(row=4)
+    Label(master, text='Search query : ').grid(row=5)
+    ip_address = Entry(master)
+    port_no = Entry(master)
+    usr_name = Entry(master)
+    password = Entry(master, show='*')
+    time_range = Entry(master)
+    search_query = Entry(master)
+    ip_address.grid(row=0, column=1)
+    port_no.grid(row=1, column=1)
+    usr_name.grid(row=2, column=1)
+    password.grid(row=3, column=1)
+    time_range.grid(row=4, column=1)
+    search_query.grid(row=5, column=1)
 
-kwargs_export = {'earliest_time': '-{}'.format(earliest_time),
-                 'latest_time': 'now',
-                 'search_mode': 'normal'}
-searchquery_export = 'search {}'.format(search_key)
+    def search():
+        service = client.connect(
+            host=ip_address.get(), port=int(port_no.get()),
+            username=usr_name.get(), password=password.get())
 
-exportsearch_results = service.jobs.export(searchquery_export, **kwargs_export)
+        kwargs_export = {'earliest_time': '-{}'.format(time_range.get()),
+                         'latest_time': 'now',
+                         'search_mode': 'normal'}
+        searchquery_export = 'search {}'.format(search_query.get())
 
-# Get the results and display them using the ResultsReader
-reader = results.ResultsReader(exportsearch_results)
+        exportsearch_results = service.jobs.export(searchquery_export,
+                                                   **kwargs_export)
 
-result_list = list()
-for result in reader:
-    if isinstance(result, dict):
-        result_list.append(result)
+        # Get the results and display them using the ResultsReader
+        reader = results.ResultsReader(exportsearch_results)
 
-print json.dumps(result_list)
+        result_list = list()
+        for result in reader:
+            if isinstance(result, dict):
+                result_list.append(result)
+        file_name = f'search-output-{datetime.now()}.json'
+        with open(file_name, "w") as outfile:
+            json.dump(result_list, outfile)
 
-#To do: send mail
+        stored_path = f'Search result stored in {os.getcwd()}/{file_name}'
+
+        child_wd = Tk()
+        Label(child_wd, text=stored_path).grid(row=1)
+        close_child_btn = Button(
+            child_wd, text="close", command=child_wd.destroy)
+        close_child_btn.grid(row=2, column=1)
+        mainloop()
+
+    search_btn = Button(master, text="Submit", command=search)
+    search_btn.grid(row=6, column=0)
+
+    close_btn = Button(master, text="close", command=master.destroy)
+    close_btn.grid(row=6, column=1)
+    mainloop()
+
+
+init_ui()
